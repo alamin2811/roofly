44f55f836c83036c52c449610de56050062204b0
alamin2811 <alaminbinmonsur@gmail.com>
2024-04-05 17:08:46 +0600

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,38 @@
             autoplaySpeed: 4000,
         });
     }
+    /*-- testimonials-slider-Start --*/
+    var testimonialsSlider = $(".testimonials-slider-for");
+    if (testimonialsSlider.length) {
+        $('.testimonials-slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            autoplay: true,
+            arrows: true,
+            fade: false,
+            autoplaySpeed: 3000,
+            asNavFor: '.testimonials-slider-nav',
+        });
+    }
+    var testimonialsSliderNavList = $(".testimonials-slider-nav");
+    if (testimonialsSliderNavList.length) {
+        $('.testimonials-slider-nav').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            asNavFor: '.testimonials-slider-for',
+            dots: false,
+            autoplay: true,
+            centerMode: false,
+            focusOnSelect: true,
+            autoplaySpeed: 4000,
+        });
+    }
 
-    var recentPostSlider = $(".recent-post-slider");
+    /*-- recent project slider --*/
+    var recentPostSlider = $(".recent-project-slider");
     if (recentPostSlider.length) {
-        $('.recent-post-slider').slick({
+        $('.recent-project-slider').slick({
             slidesToShow: 3,
             slidesToScroll: 1,
             infinite: true,
